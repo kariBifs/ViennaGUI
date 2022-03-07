ef9d862ec0adb001e6e03e0d99bb33d85403374e
anasayo <98788112+anasayo@users.noreply.github.com>
2022-03-07 07:05:49 -0500

    Updated config file
    Updated some comments on the config file

--- vienna_config_v1.py
+++ vienna_config_v1.py
@@ -4,31 +4,32 @@
 import os, sys, shutil 
 import subprocess
 
-#Find RNAfold
+#Find RNAfold executable
 find_exe = shutil.which("RNAfold")
 
 #Remove the comment for debugging
+#print (find_exe)
 #print os.path.dirname(find_exe)
 
 #Get the directory of executable
 exe_path = os.path.dirname(find_exe)
 
-#Check if directory is in PATH
+#Check if directory is in PATH, if not add to PATH
 if os.path.isdir(exe_path) == True:
    print ("ViennaRNA - RNAfold..........ok")
    
 else:
-   sys.path.insert(0, exe_path)
+   sys.path.insert(1, exe_path)
    print ("ViennaRNA - RNAfold..........added")
 
-#Check Ghostscript is installed:
+#Check Ghostscript is installed, if not suggest installation and exit:
 if shutil.which("gs") is not None:
    print ("Ghostscript..................ok")
 else:
    print ("Ghosescript not found, try\n\nsudo apt install ghostscript")
    sys.exit()
 
-#Check is modules are installed
+#Check if modules tkinter and pillow are installed, else exit
 try:
    import tkinter
    import PIL
@@ -37,7 +38,8 @@ except ModuleNotFoundError as msg:
    sys.exit()   
 
    
-#Create tmp directory if it does not exist
+#Create tmp directory and change to the new directory,
+#else change into tmp directory if already there
 current_path = os.getcwd()
 temp_dir = "tmp"
 new_path = os.path.join(current_path, temp_dir)
